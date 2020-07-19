d305a307622cbef775352a4b2723f5d00129dcbb
hopeof-Greatmind <58543696+hopeof-Greatmind@users.noreply.github.com>
2020-07-19 16:39:57 +0900

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -61,7 +61,7 @@ def split(df, group):
 
 
 def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+    with tf.compat.v1.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
