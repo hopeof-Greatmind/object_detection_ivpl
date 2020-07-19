57f3140133b20cbd7c6c380a278d7dd098098ad0
hopeof-Greatmind <58543696+hopeof-Greatmind@users.noreply.github.com>
2020-07-19 16:18:00 +0900

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -102,7 +102,7 @@ def create_tf_example(group, path):
 
 
 def main(_):
-    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    writer = tf.compat.v1.python_io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(os.getcwd(), FLAGS.image_dir)
     examples = pd.read_csv(FLAGS.csv_input)
     grouped = split(examples, 'filename')
